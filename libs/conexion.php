d9d6cb09e2c9098c3d2674e0b9985af2aa830b7a
fede064 <farrieta064@gmail.com>
2022-08-10 20:53:34 -0300

    cambio conexion

--- libs/conexion.php
+++ libs/conexion.php
@@ -2,8 +2,8 @@
 	function Conectarse(){
 		$servidor="localhost";
 		$basededatos="incompanylearning";
-		$usuario="root";
-		$clave="";
+		$usuario="fede";
+		$clave="fexarprod";
 		$cn=mysql_connect($servidor,$usuario,$clave) or die ("Error conectando a la base de datos");
 		mysql_select_db($basededatos ,$cn) or die("Error seleccionando la Base de datos");
 		mysql_query ("SET NAMES 'utf8'");
